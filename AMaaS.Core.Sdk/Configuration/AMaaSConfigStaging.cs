7ac68c53d43af0cc52d6fde56d32403698814207
paul-rs <roland.paul.santos@gmail.com>
2017-09-05 19:40:48 +0800

    AMAAS-1218 Updated cognito pool configuration

--- AMaaS.Core.Sdk/Configuration/AMaaSConfigStaging.cs
+++ AMaaS.Core.Sdk/Configuration/AMaaSConfigStaging.cs
@@ -10,9 +10,9 @@ namespace AMaaS.Core.Sdk.Configuration
 {
     public class AMaaSConfigStaging : AMaaSConfigBase
     {
-        public override string CognitoPoolId => "us-west-2_wKa82vECF";
-        public override string CognitoClientId => "5mrqm1sjmfp80k8foasq83rb9k";
-        public override string AwsRegion => "us-west-2";
+        public override string CognitoPoolId => "ap-southeast-1_De6j7TWIB";
+        public override string CognitoClientId => "2qk35mhjjpk165vssuqhqoi1lk";
+        public override string AwsRegion => "ap-southeast-1";
         public override AMaaSEnvironment Environment
         {
             get { return AMaaSEnvironment.Staging; }
